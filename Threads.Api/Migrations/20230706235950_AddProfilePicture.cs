baa7671bd2c71a4dd48ece12d5f18636f8fcc484
campos96 <cesarhcmartinez@gmail.com>
2023-07-06 17:01:47 -0700

    Api: Migrate add profile picture

---
+++ Threads.Api/Migrations/20230706235950_AddProfilePicture.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Threads.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProfilePicture : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "Picture",
+                table: "Profiles",
+                type: "varbinary(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Picture",
+                table: "Profiles");
+        }
+    }
+}
